f83ad18ab6b1d02dc7d7f491252f2541f3517323
liu2830 <2305195328@qq.com>
2023-02-21 14:49:00 +0800

    modified:   .gitignore 	new file:   bit/bit.cc 	deleted:    out/test.exe 	deleted:    out/test1.exe 	renamed:    "1.\344\270\244\346\225\260\344\271\213\345\222\214.cpp" -> "src/1.\344\270\244\346\225\260\344\271\213\345\222\214.cpp" 	modified:   test/test.cc

--- test/test.cc
+++ test/test.cc
@@ -6,20 +6,19 @@
  * @date 2022-11-25
  ********************************************************************************/
 #include <iostream>
-#include <unordered_map>
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    unordered_map<string, int> HashMap{{"Aa", 0}, {"Bb", 222}, {"Cc", 333}, {"Dd", 444}};
-    // try
-    // {
-        // HashMap.at("Cc");
-        cout << HashMap["Aa"] << endl;
-    // }
-    // catch (logic_error e)
-    // {
-    //     cout << "ERROR!" << endl;
-    // }
+    int a = 10; //bin 1010
+    int b = 12; //bin 1100
+    int c = a & b;
+    /*
+        1010
+       &
+        1100
+        1000    十进制8
+    */
+    cout << c << endl;  //c为8
     return 0;
 }
